d9addd37ab18f792c2867b6dd4997388e57d7e77
Vinicius Fantin <vinifantin302@gmail.com>
2025-03-27 18:12:33 -0300

    repo - initialize

---
+++ GitSeeker/src/components/Repo.tsx
@@ -0,0 +1,13 @@
+import { RepoProps } from "../types/Repo";
+
+import { AiOutlineStar, AiOutlineFork } from "react-icons/ai";
+import { BsCodeSlash } from "react-icons/bs";
+import { RiGitRepositoryLine } from "react-icons/ri";
+
+import style from "./Repo.module.css";
+
+const Repo = () => {
+  return <div>Repo</div>;
+};
+
+export default Repo;
