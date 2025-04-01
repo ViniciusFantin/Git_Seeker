3f82f2dcadd20df8e00e7a8569697738da2628e0
Vinicius Fantin <vinifantin302@gmail.com>
2025-04-01 18:13:22 -0300

    feat - repo insides

--- GitSeeker/src/components/Repo.tsx
+++ GitSeeker/src/components/Repo.tsx
@@ -6,8 +6,31 @@ import { RiGitRepositoryLine } from "react-icons/ri";
 
 import style from "./Repo.module.css";
 
-const Repo = () => {
-  return <div>Repo</div>;
-};
+const Repo =
+  () =>
+  ({ name, language, html_url, forks_count, stargazers_count }: RepoProps) => {
+    return (
+      <div className={style.repo}>
+        <h1>{name}</h1>
+        <p>
+          <BsCodeSlash /> {language}
+        </p>
+        <div className={style.stats}>
+          <div>
+            <AiOutlineStar />
+            <span>{stargazers_count}</span>
+          </div>
+          <div>
+            <AiOutlineFork />
+            <span>{forks_count}</span>
+          </div>
+        </div>
+        <a href={html_url} target="_blank" className={style.repo_btn}>
+          <span>Code View</span>
+          <RiGitRepositoryLine />
+        </a>
+      </div>
+    );
+  };
 
 export default Repo;
