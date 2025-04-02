f46020eeaf3f934ee649c4757af984757f79087c
Vinicius Fantin <vinifantin302@gmail.com>
2025-04-02 15:17:23 -0300

    feat - repo

--- GitSeeker/src/components/Repo.tsx
+++ GitSeeker/src/components/Repo.tsx
@@ -6,31 +6,35 @@ import { RiGitRepositoryLine } from "react-icons/ri";
 
 import style from "./Repo.module.css";
 
-const Repo =
-  () =>
-  ({ name, language, html_url, forks_count, stargazers_count }: RepoProps) => {
-    return (
-      <div className={style.repo}>
-        <h1>{name}</h1>
-        <p>
-          <BsCodeSlash /> {language}
-        </p>
-        <div className={style.stats}>
-          <div>
-            <AiOutlineStar />
-            <span>{stargazers_count}</span>
-          </div>
-          <div>
-            <AiOutlineFork />
-            <span>{forks_count}</span>
-          </div>
+const Repo = ({
+  name,
+  language,
+  html_url,
+  forks_count,
+  stargazers_count,
+}: RepoProps) => {
+  return (
+    <div className={style.repo}>
+      <h1>{name}</h1>
+      <p>
+        <BsCodeSlash /> {language}
+      </p>
+      <div className={style.stats}>
+        <div>
+          <AiOutlineStar />
+          <span>{stargazers_count}</span>
         </div>
-        <a href={html_url} target="_blank" className={style.repo_btn}>
-          <span>Code View</span>
-          <RiGitRepositoryLine />
-        </a>
+        <div>
+          <AiOutlineFork />
+          <span>{forks_count}</span>
+        </div>
       </div>
-    );
-  };
+      <a href={html_url} target="_blank" className={style.repo_btn}>
+        <span>Code View</span>
+        <RiGitRepositoryLine />
+      </a>
+    </div>
+  );
+};
 
 export default Repo;
--- GitSeeker/src/routes/Repos.tsx
+++ GitSeeker/src/routes/Repos.tsx
@@ -10,7 +10,54 @@ import styles from "./Repos.module.css";
 import Loader from "../components/Loader";
 
 const Repos = () => {
-  const { userName } = useParams();
+  const { username } = useParams();
 
-  const { repos, setRepos } = useState<RepoRepos[] | [] | null>(null);
+  const [repos, setRepos] = useState<RepoProps[] | [] | null>(null);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const loadrepos = async function (username: string) {
+      setIsLoading(true);
+
+      const res = await fetch(`https://api.github.com/users/${username}/repos`);
+
+      const data = await res.json();
+
+      setIsLoading(false);
+
+      let orderedRepos = data.sort(
+        (a: RepoProps, b: RepoProps) => b.stargazers_count - a.stargazers_count
+      );
+
+      orderedRepos = orderedRepos.slice(0, 5);
+
+      setRepos(orderedRepos);
+    };
+
+    if (username) {
+      loadrepos(username);
+    }
+  }, []);
+
+  if (!repos && isLoading) return <Loader />;
+
+  return (
+    <div className={styles.repo}>
+      <BackBtn />
+      <h2>Explore the user repositories</h2>
+      {repos && repos.length === 0 && (
+        <p>There is no Repositories in here :/ </p>
+      )}
+      {repos && repos.length > 0 && (
+        <div className={styles.repos_container}>
+          {repos.map((repo: RepoProps) => (
+            <Repo key={repo.name} {...repo} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
+
+export default Repos;
